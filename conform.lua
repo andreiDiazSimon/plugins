1eebe5fbd58ce580c857d9090e50b9a8d3144b5e
Andrei Simon <andreisimon2004@gmail.com>
2024-11-16 07:13:17 +0800

    conform

---
+++ conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			format_on_save = {
+				timeout_ms = 100, -- Timeout for formatting
+				lsp_format = "fallback", -- Use LSP formatter if no dedicated formatter is available
+			},
+			test = 1,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				-- rust = { "rustfmt", lsp_format = "fallback" },		-- Conform will run the first available formatter
+				javascript = { "prettier", "prettierd", stop_after_first = true },
+			},
+		})
+	end,
+}
